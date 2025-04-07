872ae7e079ace5b79eb114cf505d0b3c99a6d5bf
Babengkat <autentico.ira@gmail.com>
2025-04-08 03:50:22 +0800

    update

--- back_end/urls.py
+++ back_end/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/tasks/', include('todo_app.urls')),  # Connects API routes
+    path('api/', include('todo_app.urls')),  # ← this is important
 ]
